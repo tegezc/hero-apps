a52765d752ea62e37e79cf4f7409428d71af9a0a
tegezc <tegesitt@gmail.com>
2022-02-01 13:23:03 +0700

    progress tandem selling

---
+++ lib/module_mt/data/datasources/auth/get_remote_login.dart
@@ -0,0 +1,21 @@
+import 'package:dio/dio.dart';
+import 'package:hero/module_mt/data/datasources/core/dio_config.dart';
+import 'package:hero/module_mt/data/model/account_model.dart';
+
+abstract class GetRemoteLogin {
+  Future<AccountModel?> login(String id, String password);
+}
+
+class GetRemoteLoginImpl implements GetRemoteLogin {
+  final GetDio getDio = GetDio();
+  @override
+  Future<AccountModel?> login(String id, String password) async {
+    Dio dio = getDio.dioLogin();
+
+    var response = await dio.post(
+      '/auth/login',
+      data: {'username': id, 'password': password},
+    );
+    return AccountModel.fromJson(response.data);
+  }
+}
